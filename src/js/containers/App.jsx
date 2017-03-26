2a4d34be4a21c26f7f6e8f4dc94dd5cf79c474da
Lennert Blommaerte <lennert.blommaert@student.howest.be>
2017-03-26 11:08:28 +0200

    :tada: initial commit

---
+++ src/js/containers/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <section>
+
+      <header>
+        <h1>Hello, MAIV_NOSTALGIE</h1>
+      </header>
+
+    </section>
+  );
+
+};
+
+export default App;
